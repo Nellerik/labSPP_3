00bbf8829a74eb57ce374378f2a02201b94bc20e
Egor Zaprigaev <egorzap77@gmail.com>
2024-03-13 16:42:52 +0300

    rdy

--- labSPP_3/Program.cs
+++ labSPP_3/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Computers}/{action=Index}/{id?}");
 
 app.Run();
